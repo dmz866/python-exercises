97281f4b4a87e564d20adefadec2ae8c24323461
David Murillo <dmz866@hotmail.com>
2025-02-14 11:42:09 -0500

    datetime

---
+++ Bases Python/datetime-examples.py
@@ -0,0 +1,12 @@
+from datetime import datetime, date, time
+
+print(date(2025,11,28))
+print(time(17, 35))
+
+mi_date = datetime(2026,12,1, 23, 11)
+mi_date = mi_date.replace(month=5)
+print(mi_date)
+
+
+
+
