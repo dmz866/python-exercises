2f52ec60a9b661d58ca423420c1278665ce65601
David Murillo <dmz866@hotmail.com>
2025-02-17 11:46:20 -0500

    zip files

--- Bases Python/zip_files.py
+++ Bases Python/zip_files.py
@@ -1,6 +1,19 @@
 import zipfile
+import shutil
 
+'''
+#Create zip file
 zip_file = zipfile.ZipFile('zip_file.zip', 'w')
 zip_file.write('test.txt')
 zip_file.write('test2.txt')
 zip_file.close()
+
+#Unzip file
+zp = zipfile.ZipFile('zip_file.zip', 'r')
+zp.extractall()
+
+'''
+#Unzip file
+shutil.unpack_archive('zip_file.zip', 'new folder unzip name')
+
+
